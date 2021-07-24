3765be0504c79bf3bac94b5a0748811ffde1792f
Archeox <archeox2003@gmail.com>
2021-07-24 13:34:12 +0200

    Initial Commit

--- src/main/java/LGUneNuit.java
+++ src/main/java/LGUneNuit.java
@@ -14,56 +14,31 @@ import java.nio.charset.StandardCharsets;
 
 public class LGUneNuit {
 
-    private final static String TOKEN_FILE = "token.secret";
-    private final static Charset TOKEN_CHARSET = StandardCharsets.UTF_8;
-    private String test;
-
 
     public static void main(String[] args) {
 
-        try {
-            GatewayDiscordClient client = DiscordClientBuilder.create(readToken())
-                    .build()
-                    .login()
-                    .block();
+        GatewayDiscordClient client = DiscordClientBuilder.create(System.getenv("token"))
+                .build()
+                .login()
+                .block();
 
-            client.getEventDispatcher().on(ReadyEvent.class)
-                    .subscribe(event -> {
-                        final User self = event.getSelf();
-                        System.out.println(String.format(
-                                "\u001B[32mLogged in as %s#%s\u001B[0m", self.getUsername(), self.getDiscriminator()
-                        ));
-                    });
+        client.getEventDispatcher().on(ReadyEvent.class)
+                .subscribe(event -> {
+                    final User self = event.getSelf();
+                    System.out.println(String.format(
+                            "\u001B[32mLogged in as %s#%s\u001B[0m", self.getUsername(), self.getDiscriminator()
+                    ));
+                });
 
-            client.getEventDispatcher().on(MessageCreateEvent.class)
-                    .map(MessageCreateEvent::getMessage)
-                    .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-                    .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
-                    .flatMap(Message::getChannel)
-                    .flatMap(channel -> channel.createMessage("Pong!"))
-                    .subscribe();
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Pong!"))
+                .subscribe();
 
-            client.onDisconnect().block();
-        } catch (IOException e) {
-            System.err.println("Error while reading the token file");
-        }
-    }
-
-    private static String readToken() throws IOException {
-
-        BufferedReader br = null;
-        String result = null;
-        try {
-            br = new BufferedReader(
-                    new InputStreamReader(
-                            new FileInputStream(TOKEN_FILE), TOKEN_CHARSET));
-
-            result = br.readLine();
-        } catch (Exception e) {
-            throw new IOException("Error while reading the token file");
-        }
-        return result;
-
+        client.onDisconnect().block();
     }
 
 }
